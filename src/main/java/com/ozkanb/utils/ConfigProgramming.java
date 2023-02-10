9260c4430ba25699f27c716556213411381f2cbc
ozkan.baltaoglu <495551@egm.gov.tr>
2023-02-10 17:25:26 +0300

    DevTools Aktif Etmek

---
+++ src/main/java/com/ozkanb/utils/ConfigProgramming.java
@@ -0,0 +1,8 @@
+package com.ozkanb.utils;
+
+public class ConfigProgramming {
+
+    //DevTools ==> LiveLoad
+    //file ==> setting ==>complier ==> Build project automatically ( OK clicked) (Ctrl+Alt+S)
+    //file ==> settings ==> Advanced Settingd ==> Allow Auto-make to start even ( OK clicked)
+}
